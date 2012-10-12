c1d4c9cc4880350d32ea76428957c2916a9e9a66
Emmanuel Cagadas <emmanuel.cagadas@gmail.com>
2012-10-12 23:41:33 +0800

    hw2 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,16 +7,38 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Hash.new
+    Movie.all_ratings.each {|x| @all_ratings[x.to_sym] = true}
+    @title_href, @release_date_href = {:sort => 'title'}, {:sort => 'release_date'}
+    
     @movies = case params[:sort]
-    when 'title'
-      @title_hilite = 'hilite'
-      Movie.order('title').all
-    when 'release_date'
-      @release_date_hilite = 'hilite'
-      Movie.order('release_date').all
-    else
-      Movie.all
-    end
+              when 'title'
+                @title_hilite = 'hilite'
+                Movie.order('title')
+              when 'release_date'
+                @release_date_hilite = 'hilite'
+                Movie.order('release_date')
+              else
+                Movie
+              end
+
+
+    @movies = if params[:commit] == 'Refresh'
+                if params[:ratings]
+                  checked_ratings = params[:ratings].keys.map {|x| x.to_sym}
+                  (@all_ratings.keys - checked_ratings).each {|x| @all_ratings[x] = false}
+                  temp_hash = {:commit => 'Refresh'}
+                  checked_ratings.each {|x| temp_hash["ratings[#{x}]".to_sym] = 1}
+                  @title_href.merge! temp_hash
+                  @release_date_href.merge! temp_hash
+                  @movies.find_all_by_rating(checked_ratings)
+                else
+                  @all_ratings.each {|key,value| @all_ratings[key] = false}
+                  []
+                end
+              else
+                @movies.all
+              end
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    self.select("DISTINCT(rating)").map { |x| x.rating }.sort
+  end
 end
